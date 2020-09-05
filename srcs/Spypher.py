bbbfc58e38d2f2ea1c2a79e8e2b68b749be828f7
Ryan Parker <ryan@ryanparker.org>
2020-09-05 14:50:02 -0500

    ENH: Added decryption of Casear Ciphers

--- srcs/Spypher.py
+++ srcs/Spypher.py
@@ -1,6 +1,6 @@
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def encryptCeaser(msg, shift):
+def encryptCaesar(msg, shift):
     result = ""
     for i in range(len(msg)):
         letter = msg[i]
@@ -15,6 +15,21 @@ def encryptCeaser(msg, shift):
     return result
 
 
+def decryptCaesar(msg, shift):
+    result = ""
+    for i in range(len(msg)):
+        letter = msg[i]
+
+        if letter not in alphabet:
+            result += letter
+            continue
+        elif letter.isupper():
+            result += chr((ord(letter) - shift - 65) % 26 + 65)
+        else:
+            result += chr((ord(letter) - shift - 97) % 26 + 97)
+    return result
+
+
 print("Welcome to Spypher!")
 print("Please select an option:")
 print("\tE: Encrypt")
@@ -26,11 +41,21 @@ if userInput.upper() == "E":
     message = str(input("Please enter message to encrypt:\n"))
     print("Please select a method to use:")
     print("\tC: Caesar Cipher")
+    print("\tB: Base64 Encryption")
 
     userInput = str(input())
 
     if userInput.upper() == "C":
         shiftNum = int(input("Please enter number to shift by:\n"))
-        print(encryptCeaser(message, shiftNum))
+        print(encryptCaesar(message, shiftNum))
 
+if userInput.upper() == "D":
+    message = str(input("Please enter message to decrypt:\n"))
+    print("Please select a method to use:")
+    print("\tC: Caesar Cipher")
 
+    userInput = str(input())
+
+    if userInput.upper() == "C":
+        shiftNum = int(input("Please enter the number the message is shifted by:\n"))
+        print(decryptCaesar(message, shiftNum))
